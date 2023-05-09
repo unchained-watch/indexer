245d18f427a26e2300e30ae38f5029ec3469d5b4
Olrem <valentin@boleromusic.com>
2023-05-09 23:59:24 +0200

    feat: add function error abi parsing

--- core/src/controller/process.rs
+++ core/src/controller/process.rs
@@ -20,44 +20,8 @@ pub async fn get_history(
         Err(error) => panic!("Error : {:?}", error),
     };
 
-    let mut signatures: Vec<Event> = vec![];
-    for event_str in parse_abi(abi_path).unwrap().iter() {
-        let mut signature_event = String::new();
-        signature_event.push_str(&event_str.name);
-
-        if let Some(value) = &event_str.str {
-            signature_event.push_str(&value);
-        } else {
-            signature_event.push_str("()");
-        }
-
-        let mut keccak = Keccak::v256();
-        let mut output = [0u8; 32];
-        keccak.update(signature_event.as_bytes());
-        keccak.finalize(&mut output);
-
-        // Concat bytes array
-        let mut signature_str = String::new();
-        for byte in output.iter() {
-            signature_str.push_str(&format!("{:02x}", byte));
-        }
-        let event = Event {
-            id: None,
-            name: signature_event,
-            signature: signature_str,
-            json: event_str.json.clone().unwrap(),
-            contract_address: contract_address.to_string(),
-        };
-
-        match create(&event).await {
-            Ok(_) => (),
-            Err(error) => panic!("Error when saving data {}", error),
-        };
-
-        signatures.push(event);
-    }
-
-    listeners::get_past_events(&contract_address, &signatures, &block_number).await?;
+   
+   // listeners::get_past_events(&contract_address, &signatures, &block_number).await?;
     Ok(())
 }
 
@@ -65,42 +29,10 @@ pub async fn get_abi(
     contract_address: String,
     abi_path: std::path::PathBuf,
 ) -> Result<(), web3::Error> {
-    let mut signatures: Vec<Event> = vec![];
-    for event_str in parse_abi(abi_path).unwrap().iter() {
-        let mut signature_event = String::new();
-        signature_event.push_str(&event_str.name);
 
-        if let Some(value) = &event_str.str {
-            signature_event.push_str(&value);
-        } else {
-            signature_event.push_str("()");
-        }
-
-        let mut keccak = Keccak::v256();
-        let mut output = [0u8; 32];
-        keccak.update(signature_event.as_bytes());
-        keccak.finalize(&mut output);
-
-        // Concat bytes array
-        let mut signature_str = String::new();
-        for byte in output.iter() {
-            signature_str.push_str(&format!("{:02x}", byte));
-        }
-        let event = Event {
-            id: None,
-            name: signature_event,
-            signature: signature_str,
-            json: event_str.json.clone().unwrap(),
-            contract_address: contract_address.to_string(),
-        };
-
-        match create(&event).await {
-            Ok(_) => (),
-            Err(error) => panic!("Error when saving data {}", error),
-        };
-
-        signatures.push(event);
-    }
+    
+    parse_abi(abi_path, &contract_address).await?;
+       
     Ok(())
 }
 
